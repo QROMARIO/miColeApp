b73adbc64645ba9060dde51811138c6df5c3e86c
Romario Quispe <qromario@gmail.com>
2024-04-16 22:28:23 -0500

    Web MiCole

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls:
+  [
+    './app.component.css',
+    './app.responsive.css',
+    './app.bootstrap.css'
+
+  ]
 })
 export class AppComponent {
   title = 'miColeApp';
