c71740128072eb69fe0ff31bc1357b450e8f9c6f
Romario Quispe <qromario@gmail.com>
2024-04-20 04:14:22 -0500

    video genexus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YouTubePlayer } from '@angular/youtube-player';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    YouTubePlayer,
   ],
   providers: [],
   bootstrap: [AppComponent]
